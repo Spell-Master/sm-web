62176d231d19716e87699cb12b2dbc0add90a055
Spell Master <spellmastergames@gmail.com>
2018-08-15 22:54:26 -0300

    css v2.1

--- app_script/ajax/files/teste.js
+++
@@ -1,4 +0,0 @@
-function testeA() {
-    document.getElementById('teste-de-function').innerText = 'Executando função no arquivo aberto pelo ajax,'
-            + ' Essa função está em um arquivo js requisitado no index';
-}
--- app_script/ajax/files/testeb.js
+++
@@ -1,4 +0,0 @@
-function testeB() {
-    document.getElementById('teste-de-function').innerText = 'Executando função no arquivo aberto pelo ajax,'
-            + ' Essa função está em um arquivo js requisitado no próprio arquivo';
-}

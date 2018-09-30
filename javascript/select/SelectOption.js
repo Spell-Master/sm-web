4f3eb547b57bb28919df9aec4a360890e5c43437
Spell Master <spellmastergames@gmail.com>
2018-09-29 23:10:39 -0300

    SelectOption v-3.0

--- javascript/select/SelectOption.js
+++ javascript/select/SelectOption.js
@@ -1,111 +1,122 @@
 /**
  * ****************************************************
- * * @Class Select
- * * @author Spell-Master (Omar Pautz)
- * * @version 2.0 (2018)
+ * @Copyright (c) 2017, Spell Master.
+ * @version 3.0 (2018)
+ * @requires  Navegador compatível com HTML 5
  * ****************************************************
- * * @Info Personaliza grupo de opções 
- * ****************************************************
- * @param id : Id do select a ser personalizado
+ * @class Personaliza grupo de opções.
  * ****************************************************
  */
 
-var SelectOption = function (id) {
-    var $id = id;
-    var $trg = document.getElementById($id);
-    var $new, $head, $ul, $li, $i, $opt, $next, $cT;
+var SelectOption = function () {
+
+    var $select = document.getElementsByClassName('selected'), $idx, $current, $base, $button, $ul, $li, $head, $opt, $j, $active, $target;
     createNew();
 
     /**
      * ************************************************
-     * * @function Cria os elementos base para o
-     * select personalizado
+     * * @private : Cria um novo elemento para
+     * abrigar o novo select
      * ************************************************
      */
     function createNew() {
-        $new = document.createElement('div');
-        $new.classList.add('slt');
-        $head = document.createElement('div');
-        $head.classList.add('slt-title');
-        $trg.parentNode.insertBefore($new, $trg.nextSibling);
-        $new.appendChild($head);
+        for ($idx = 0; $idx < $select.length; $idx++) {
+            $current = $select[$idx];
+            $base = document.createElement('div');
+            $base.classList.add('select-base');
+            $current.parentNode.insertBefore($base, $current);
+            createButton();
+        }
+    }
+
+    /**
+     * ************************************************
+     * * @private : Cria o botão de controle
+     * ************************************************
+     */
+    function createButton() {
+        $button = document.createElement('div');
+        $button.classList.add('select-button');
+        $base.appendChild($button);
         createList();
     }
 
     /**
      * ************************************************
-     * * @function Cria a estrutura da lista
+     * * @private : Cria um elemento de lista
+     * para o novo select
      * ************************************************
      */
     function createList() {
         $ul = document.createElement('ul');
-        $ul.classList.add('slt-ul');
-        $new.appendChild($ul);
-        $new.addEventListener('click', toggleList);
-        createOptions();
+        $ul.classList.add('select-list');
+        $button.parentNode.insertBefore($ul, $button.nextSibling);
+        queryOptions();
     }
 
     /**
      * ************************************************
-     * * @function Obtem as opções do select e adiona
-     * as mesmas para os personalizados
+     * * @private : Obtem os valores do select e
+     * aplica os mesmos ao novo select
      * ************************************************
      */
-    function createOptions() {
-        $opt = $trg.querySelectorAll('option');
-        for ($i = 0; $i < $opt.length; $i++) {
+    function queryOptions() {
+        if ($head == undefined) {
+            $head = document.getElementsByClassName('select-button');
+        }
+        $opt = $current.querySelectorAll('option');
+        for ($j = 0; $j < $opt.length; $j++) {
             $li = document.createElement('li');
-            $li.setAttribute('data-select', $opt[$i].value);
-            $li.innerText = $opt[$i].innerText;
+            $li.setAttribute('data-select', $opt[$j].value);
+            $li.setAttribute('data-parent', $idx);
+            $li.innerText = $opt[$j].innerText.substring(0, 20);
             $li.addEventListener('click', clickItem.bind(this));
             $ul.appendChild($li);
         }
-        $head.innerText = $opt[0].innerText;
+        $head[$idx].innerText = $opt[0].innerText.substring(0, 20);
     }
 
     /**
      * ************************************************
-     * * @function 
-     * Mostra a lista personalizada quando clicado
-     * sobre o cabeçalho dela
-     * * @param e : Obtem o atual elemento
-     * ************************************************
-     */
-    function toggleList(e) {
-        //e.stopPropagation();
-        $next = e.target.nextElementSibling;
-        $ul.classList.add('active');
-        $head.classList.add('sarrow');
-    }
-
-    /**
-     * ************************************************
-     * * @function Adiciona o valor item clicado ao
-     * value do select
-     * * @param e : Atual elemento
+     * * @private : Modifica os dados quando uma das
+     * opções é clicada.
+     * * @param e : Evento disparado
      * ************************************************
      */
     function clickItem(e) {
-        e.stopPropagation();
-        $cT = e.currentTarget;
-        $head.innerText = $cT.innerText;
-        $trg.value = $cT.dataset.select;
-        $cT.parentNode.classList.remove('active');
-        $head.classList.remove('sarrow');
+        var $cT = e.currentTarget;
+        var $dataSet = [$cT.dataset.parent, $cT.dataset.select];
+        $head[$dataSet[0]].innerText = $cT.innerText.substring(0, 20);
+        $select[$dataSet[0]].value = $dataSet[1];
     }
 
     /**
      * ************************************************
-     * * @function Quando clicado fora do select
-     * personalizado oculta os elementos
+     * * @private : Controle a exibição dos selects
+     * * @param event : Evento disparado
      * ************************************************
      */
-    function closeOff() {
-        var $tol = document.getElementsByClassName('slt-ul');
-        for ($i = 0; $i < $tol.length; $i++) {
-            $tol[$i].classList.remove('active');
+    function clickDoc(event) {
+        if (event.which === 1) {
+            $target = event.target;
+            if ($active === $target) {
+                $active.classList.remove('active');
+                $target.nextElementSibling.classList.remove('active');
+                $active = null;
+            } else if ($target.className === 'select-button') {
+                if ($active) {
+                    $active.classList.remove('active');
+                    $active.nextElementSibling.classList.remove('active');
+                }
+                $target.classList.add('active');
+                $target.nextElementSibling.classList.add('active');
+                $active = $target;
+            } else if ($active != undefined || $active != null) {
+                $active.classList.remove('active');
+                $active.nextElementSibling.classList.remove('active');
+                $active = null;
+            }
         }
-        $head.classList.remove('sarrow');
     }
-    document.addEventListener('click', closeOff, true);
+    document.addEventListener('click', clickDoc, false);
 };

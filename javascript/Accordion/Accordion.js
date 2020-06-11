0ec859852f8ddea57cd2b221e2ac408966bcba11
Spell Master <spellmastergames@gmail.com>
2020-06-11 12:35:31 -0300

    Accordion v2.1

--- javascript/Accordion/Accordion.js
+++ javascript/Accordion/Accordion.js
@@ -1,21 +1,31 @@
 /**
  * ****************************************************
- * @Copyright (c) 2016, Spell Master.
- * @Version: 2.0
+ * * @Class Accordion
+ * * @author Spell-Master (Omar Pautz)
+ * * @copyright 2018
+ * * @version 2.1 (2020)
  * ****************************************************
- * @class Executa efeito sanfona em seletores.
+ * * Executa efeito sanfona em elementos
  * ****************************************************
- **/
+ */
+
 var Accordion = function () {
 
-    var $button = document.getElementsByClassName('acc-button'), $target, $next, $copy, $h, $last;
+    var $button = document.getElementsByClassName('acc-button');
+    var $this = {
+        'target': null,
+        'next': null,
+        'last': null,
+        'copy': null,
+        'height': null
+    };
 
     queryButtons();
 
     /**
      * ************************************************
-     * * Obtem os botões do cabeçalho
-     * e adiciona o evento click em cada.
+     * @Method: Obtem os botões do cabeçalho
+     *  e adiciona o evento click em cada.
      * ************************************************
      */
     function queryButtons() {
@@ -26,19 +36,20 @@ var Accordion = function () {
 
     /**
      * ************************************************
-     * * Aciona as funções para expandir ou
-     * contrair os elementos.
-     * @param e : Referencio ao evento do elemento
-     * clicado.
+     * @Method: Aciona as funções para expandir ou
+     *  contrair os elementos.
+     * @param {OBJ} e 
+     * Referencia ao evento do elemento
+     *  clicado.
      * ************************************************
      */
     function togleContainer(e) {
-        $target = e.target;
-        $next = $target.nextElementSibling;
-        if ($last == $next) {
+        $this.target = e.target;
+        $this.next = $this.target.nextElementSibling;
+        if ($this.last == $this.next) {
             closeOpen();
         } else {
-            if ($last) {
+            if ($this.last) {
                 closeOpen();
             }
             cloneNode();
@@ -48,46 +59,47 @@ var Accordion = function () {
 
     /**
      * ************************************************
-     * * Cria temporariamente um elemento
-     * cópia do alvo na expansão para obter sua altura
+     * @Method: Cria temporariamente um elemento
+     *  cópia do alvo na expansão para obter sua altura
      * ************************************************
      */
     function cloneNode() {
-        $copy = $next.cloneNode(true);
-        $copy.setAttribute('style', 'height:auto; visibility:visible');
-        $next.parentNode.appendChild($copy);
-        $h = $copy.offsetHeight;
-        $next.parentNode.removeChild($copy);
+        $this.copy = $this.next.cloneNode(true);
+        $this.copy.setAttribute('style', 'height:auto; visibility:visible');
+        $this.next.parentNode.appendChild($this.copy);
+        $this.height = $this.copy.offsetHeight;
+        $this.next.parentNode.removeChild($this.copy);
     }
 
     /**
      * ************************************************
-     * * Contrai o elemento que estiver
-     * expandido
+     * @Method: Contrai o elemento que estiver
+     *  expandido.
      * ************************************************
      */
     function closeOpen() {
-        $last.previousElementSibling.classList.remove('active');
-        $last.style.height = 0 + 'px';
-        $last = null;
-        $h = 0;
+        $this.last.previousElementSibling.classList.remove('active');
+        $this.last.style.height = 0 + 'px';
+        $this.last = null;
+        $this.height = 0;
     }
 
     /**
      * ************************************************
-     * * Expande o elemento alvo
+     * @Method: Expande o elemento alvo
      * ************************************************
      */
     function openTarget() {
-        $target.classList.add('active');
-        $next.style.height = $h + 'px';
-        $last = $next;
+        $this.target.classList.add('active');
+        $this.next.style.height = $this.height + 'px';
+        $this.last = $this.next;
     }
 
     /**
      * ************************************************
-     * * Forca a expansão de um elemento.
-     * @param p : Índice do elemento para expandir
+     * @Method: Forca a expansão de um elemento.
+     * @param {INT} p
+     *  Índice do elemento para expandir
      * ************************************************
      */
     this.forceOpen = function (p) {
@@ -97,9 +109,9 @@ var Accordion = function () {
         } else {
             $n = 0;
         }
-        $next = document.getElementsByClassName('acc-container')[$n];
-        $last = $next;
-        $target = $next.previousElementSibling;
+        $this.next = document.getElementsByClassName('acc-container')[$n];
+        $this.last = $this.next;
+        $this.target = $this.next.previousElementSibling;
         cloneNode();
         openTarget();
     };
